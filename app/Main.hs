a59adcb1564f9f98125c69b8cbc5d8c78dfb13df
Valentin Pecatte <v.pecatte@gmail.com>
2018-12-04 15:36:10 +0100

    ref: refactor code to Graph module

--- app/Main.hs
+++ app/Main.hs
@@ -1,60 +1,16 @@
 module Main where
 
-import qualified Data.Foldable      as F
+-- import qualified Data.Foldable      as F
 import qualified Data.List          as L
-import qualified Data.Tuple         as T
+-- import qualified Data.Tuple         as T
 import qualified System.Environment as Environment
-import qualified System.IO          as IO
-
--- |A graph type
-data G = G { getNVertices :: Int          -- the number of vertices
-           , getNEdges    :: Int          -- the number of edges
-           , getEdges     :: [(Int, Int)] -- the edges 
-           } deriving (Show)
-
--- |'emptyG' return an empty graph
-emptyG :: G
-emptyG = G { getNVertices = 0, getNEdges = 0, getEdges = [] }
-
--- |'checkG' 'g' returns 'True' if the graph 'g' is valid.
--- A graph is consider valid if (not sure that this is the case for PACE 2019)
---   1) the number of vertices matches the naximum vertex among the edges
---   2) the number of edges matches the number of stored edges
---   3) the graph does not contain duplicated edges
---   4) the graph does not contain self loops
-checkG :: G -> Bool
-checkG g = checkNVertices g && checkNEdges g && checkDuplicate g && checkEdges g
-  where
-    checkNVertices g = getNVertices g == F.maximum (L.map (uncurry max) $ getEdges g)
-    checkNEdges    g = getNEdges g == L.length (getEdges g)
-    checkDuplicate g = L.length (getEdges g) == L.length (L.nub $ getEdges g)
-    checkEdges       = F.all (uncurry (/=)) . getEdges
-
--- |'stringToInt' 's' converts the string 's' to integer
-stringToInt :: String -> Int
-stringToInt s = read s :: Int
-
--- Convenience function.
-whenMaybe :: a -> Bool -> Maybe a
-whenMaybe _ False = Nothing
-whenMaybe a True  = Just a
-
--- |'parseG' 's' parses a graph given as a PACE 19 format list of strings.
--- The function returns 'Nothing' in case of parse error.
-parseG :: [String] -> Maybe G
-parseG = go emptyG . L.map L.words . L.filter (not . L.isPrefixOf "c")
-  where
-    go g []                    = whenMaybe g (checkG g)
-    go g (["p","td",n,m] : ls) = go (g { getNVertices = stringToInt n, getNEdges = stringToInt m }) ls
-    go g ([i,j]          : ls)
-      | i < j                  = go (g { getEdges = (stringToInt i, stringToInt j) : getEdges g  }) ls
-      | otherwise              = go (g { getEdges = (stringToInt j, stringToInt i) : getEdges g  }) ls
-    go _ _                     = Nothing
+import qualified System.IO as IO
+import qualified Graph as G
 
 -- Parse and display a graph.
 main :: IO ()
 main = do
   args    <- Environment.getArgs
   content <- IO.readFile (L.head args)
-  IO.putStr . show . parseG $ L.lines content
+  IO.putStr . show . G.parseG $ L.lines content
   IO.putStr "\n"
