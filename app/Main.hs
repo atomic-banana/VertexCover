6eb6c2d2ce3b83bd7d349f723d38d659e48c3fd1
Valentin Pecatte <v.pecatte@gmail.com>
2019-01-05 12:12:36 +0100

    dev: update main to read a graph file

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,19 @@
 module Main where
 
--- import qualified Data.Foldable      as F
 import qualified Data.List          as L
--- import qualified Data.Tuple         as T
 import qualified System.Environment as Environment
-import qualified System.IO as IO
-import qualified Graph as G
+import qualified System.IO          as IO
+import qualified Graph              as G
+import qualified VertexCover        as V
 
--- Parse and display a graph.
 main :: IO ()
 main = do
-  args    <- Environment.getArgs
-  content <- IO.readFile (L.head args)
-  IO.putStr . show . G.parseG $ L.lines content
-  IO.putStr "\n"
+    args    <- Environment.getArgs
+    content <- IO.readFile (L.head args)
+    IO.putStr . tryTo . G.parseG $ L.lines content
+    IO.putStr "\n"
+
+tryTo :: Maybe G.G -> String
+tryTo (Just g)      = "Correct " ++ (show $ V.select g 4)
+tryTo (Nothing)     = "Error, no graph parsed"
+
